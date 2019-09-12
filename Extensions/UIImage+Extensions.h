8c181c54bc62f5539379d0b283a7d2dc19b20f94
1145811215@qq.com <1145811215@qq.com>
2019-09-12 16:10:33 +0800

    调用拍照和相册选取功能

---
+++ Extensions/UIImage+Extensions.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+Extensions.h
+//  intelligentclass
+//
+//  Created by LiuRex on 2019/9/12.
+//  Copyright © 2019 RexLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (Extensions)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Extensions/UIImage+Extensions.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extensions.m
+//  intelligentclass
+//
+//  Created by LiuRex on 2019/9/12.
+//  Copyright © 2019 RexLiu. All rights reserved.
+//
+
+#import "UIImage+Extensions.h"
+
+@implementation UIImage (Extensions)
+
+@end
