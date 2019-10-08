7fcb0a6d6d188cab4fd37a74317257b9def2c856
1145811215@qq.com <1145811215@qq.com>
2019-10-08 09:56:36 +0800

    h5版本替换至1.9.2

---
+++ dist/static/config.js
@@ -0,0 +1,10 @@
+var appConfig = {
+  mainServerPort: "50113",
+  gatewaySeverPort: "50120",
+  mainSignalRPort: "50114",
+  gatewaySignalRPort: "50118",
+  errorLog: "development",
+  showLog: false,
+  isTest: false,
+  editorPictureMaxNum: 3
+};
