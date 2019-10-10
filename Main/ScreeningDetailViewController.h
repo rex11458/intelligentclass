03d3f6c872f51188dac59161d13e47558ec476ec
1145811215@qq.com <1145811215@qq.com>
2019-10-10 17:37:41 +0800

    正在投屏页面

---
+++ Main/ScreeningDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ScreeningDetailViewController.h
+//  intelligentclass
+//
+//  Created by LiuRex on 2019/10/10.
+//  Copyright © 2019 RexLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScreeningDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Main/ScreeningDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  ScreeningDetailViewController.m
+//  intelligentclass
+//
+//  Created by LiuRex on 2019/10/10.
+//  Copyright © 2019 RexLiu. All rights reserved.
+//
+
+#import "ScreeningDetailViewController.h"
+
+@interface ScreeningDetailViewController ()
+
+@end
+
+@implementation ScreeningDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
