e7126a8d8e5a44c3c9ff7f4b8ab42632a9200fda
1145811215@qq.com <1145811215@qq.com>
2019-12-10 16:04:16 +0800

    投屏端口号修改

--- Main/RootViewController.m
+++ Main/RootViewController.m
@@ -384,7 +384,7 @@ static RootViewController  *g_rootViewController = nil;
     if(!host){
         return ;
     }
-    [_socketManager connetHosts:@[host] port:9112];
+    [_socketManager connetHosts:@[host] port:9111];
 }
 
 
@@ -400,7 +400,7 @@ static RootViewController  *g_rootViewController = nil;
 - (void)openMultiPointPrj:(NSString *)ips{
      [self sendToPath:nil];
     [self openSendScreenWithUIHidden:YES];
-    [_socketManager connetHosts:[ips componentsSeparatedByString:@","] port:9112];
+    [_socketManager connetHosts:[ips componentsSeparatedByString:@","] port:9111];
 
 }
 
