0230d2df3cb06a2efb488c495140546294e8c14f
1145811215@qq.com <1145811215@qq.com>
2020-06-16 09:34:18 +0800

    替换h5 2.4.1

--- dist/static/config.js
+++ dist/static/config.js
@@ -1,11 +1,19 @@
-var appConfig = {
+// eslint-disable-next-line no-unused-vars
+const appConfig = {
+  // 主服务API端口
   mainServerPort: "50113",
-  gatewaySeverPort: "50120",
+  // 主服务signalR端口
   mainSignalRPort: "50114",
+
+  // 教室服务API端口
+  gatewaySeverPort: "50120",
+  // 教室服务signalR端口
   gatewaySignalRPort: "50118",
+  
   errorLog: "development",
-  SignalRHeartTime: 1000,
-  showLog: true,
+  showLog: false,
   isTest: false,
+
+  // 富文本最多添加图片数
   editorPictureMaxNum: 3
 };
